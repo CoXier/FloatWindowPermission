d857bc99bef9cec42c57752620c3bfc00f537373
lijianxin <lijianxin@bytedance.com>
2017-08-18 00:15:04 +0800

    support emui
    Change-Id: I1b9f4331dd61aea424bad7de6e8344e895527688

--- library/src/main/java/com/hust/coxier/library/rom/EmuiRom.java
+++ library/src/main/java/com/hust/coxier/library/rom/EmuiRom.java
@@ -1,16 +1,71 @@
 package com.hust.coxier.library.rom;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
 /**
  * Created by lijianxin on 2017/8/16.
  */
 
 public class EmuiRom extends Rom {
+    private static final String TAG = "EmuiRom";
 
     @Override
     public void requestFloatPermission(Context context) {
-        super.requestFloatPermission(context);
+        if (Build.VERSION.SDK_INT >= 23) {
+            super.requestFloatPermission(context);
+            return;
+        }
+        try {
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ComponentName comp = new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.addviewmonitor.AddViewMonitorActivity");//悬浮窗管理页面
+            intent.setComponent(comp);
+            if (getEmuiVersion() == 3.1) {
+                //emui 3.1 的适配
+                context.startActivity(intent);
+            } else {
+                //emui 3.0 的适配
+                comp = new ComponentName("com.huawei.systemmanager", "com.huawei.notificationmanager.ui.NotificationManagmentActivity");//悬浮窗管理页面
+                intent.setComponent(comp);
+                context.startActivity(intent);
+            }
+        } catch (SecurityException e) {
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ComponentName comp = new ComponentName("com.huawei.systemmanager",
+                    "com.huawei.permissionmanager.ui.MainActivity");
+            intent.setComponent(comp);
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ComponentName comp = new ComponentName("com.Android.settings", "com.android.settings.permission.TabItem");
+            intent.setComponent(comp);
+            context.startActivity(intent);
+            e.printStackTrace();
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    /**
+     * 获取 emui 版本号
+     * @return
+     */
+    public static double getEmuiVersion() {
+        try {
+            String emuiVersion = getSystemProperty(KEY_VERSION_EMUI);
+            String version = emuiVersion.substring(emuiVersion.indexOf("_") + 1);
+            return Double.parseDouble(version);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 4.0;
     }
 
 }
